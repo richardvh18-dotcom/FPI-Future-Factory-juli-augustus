d92a74d72f57a2d37fe43487771bb11abc60a939
richardvh18-dotcom <245593179+richardvh18-dotcom@users.noreply.github.com>
2026-08-20 07:42:24 +0000

    feat: add capacitor android support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.futurefactory.fpi',
+  appName: 'Future Factory FPI',
+  webDir: 'dist'
+};
+
+export default config;
